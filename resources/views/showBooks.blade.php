d1fd2d1addadaca66cfe7912465a993827343387
mendelor <33259548+mendelor@users.noreply.github.com>
2021-03-26 20:05:52 +1300

    Update showBooks.blade.php

--- resources/views/showBooks.blade.php
+++ resources/views/showBooks.blade.php
@@ -3,14 +3,13 @@
 <head>
 </head>
 <body>
-
+<h1>It works!!!!!!!!!<h1>
 <table>
 @foreach($books as $book)
 <tr>
     <td>{{$book->Book_name}}</td>
     <td>{{$book->Author}}</td>
     <td>{{$book->Rating}}</td>
-    <td>Change1</td>
 </tr>
 @endforeach
 </table>
