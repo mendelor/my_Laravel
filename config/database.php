d651cb53421ad5072bb3ecaf58798d4bebf701bd
mendelor <33259548+mendelor@users.noreply.github.com>
2021-04-03 00:22:02 +1300

    Update database.php

--- config/database.php
+++ config/database.php
@@ -45,12 +45,12 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'url' => ${{ secrets.DB_CONNECTION }} , 
+            'host' => ${{ secrets.DB_HOST }} ,   
+            'port' => ${{ secrets.DB_PORT }} ,
+            'database' => ${{ secrets.DB_DATABASE }} ,
+            'username' => ${{ secrets.DB_USERNAME }} ,
+            'password' => ${{ secrets.DB_PASSWORD }} ,
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
